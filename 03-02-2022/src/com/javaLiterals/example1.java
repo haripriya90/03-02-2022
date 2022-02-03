fdfd604f7b8c2c26b79160a2f254110b75d2c158
Lenovo <Lenovo@SBSH>
2022-02-03 16:46:26 +0530

    com.javaLiterals package added into project 03-02-2022

---
+++ 03-02-2022/src/com/javaLiterals/example1.java
@@ -0,0 +1,31 @@
+package com.javaLiterals;
+
+public class example1
+{
+
+	public static void main(String[] args)
+	{
+		int learnerId=1005;
+		String learnerName="priya";
+		int learnerAge=21;
+		float learnerMarks=75.05f;
+		String learnerAddress="hyderabad";
+		
+		System.out.print("learnerId: ");
+		System.out.println(learnerId);
+
+		
+		System.out.print("learnerName: ");
+		System.out.println(learnerName);
+		
+		System.out.print("learnerAge: ");
+		System.out.println(learnerAge);
+
+		
+		System.out.print("learnerMarks: ");
+		System.out.println(learnerMarks);
+
+		System.out.print("learnerAddress: ");
+		System.out.println(learnerAddress);
+	}
+}
